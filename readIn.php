76e3d1ef8bb9abb450e3dcbc064dcc549ed441e0
CorvusAtrox <john-tollefson@uiowa.edu>
2014-09-17 18:16:45 -0500

    Today's changes

--- readIn.php
+++ readIn.php
@@ -7,7 +7,15 @@ if (is_dir($dir)){
 		if(pathinfo($file, PATHINFO_EXTENSION) == 'dbf'){
 			$myf = $dir . '/' . $file;
 			$myfile = fopen($myf, "r") or die("Unable to open file!");
-			echo fread($myfile,filesize($myf));
+			//echo fread($myfile,filesize($myf));
+			$filecontent = file_get_contents($myf);
+			$chars = preg_split('//', $filecontent, -1, PREG_SPLIT_NO_EMPTY);
+			for ($i = 0; $i < count($chars); $i++) {
+				echo "<pre>";
+				//echo strcmp($chars[$i],'Á');
+				echo($chars[$i]);
+				echo "</pre>";
+			}
 			fclose($myfile);
 			echo "<br>";
 		} else {
